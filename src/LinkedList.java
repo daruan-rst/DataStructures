f01d60fdb151b028c7152b8c60562a2b682b9c2f
daruan-rst <daruan.rst@gmail.com>
2023-03-23 22:37:16 -0300

    Linked List

--- src/LinkedList.java
+++ src/LinkedList.java
@@ -1,3 +1,44 @@
+import java.util.NoSuchElementException;
+
 public class LinkedList {
-    //TODO
+
+    private Node head;
+    private int size;
+
+    public LinkedList() {
+        head = null;
+        size = 0;
+    }
+
+    public void addFirst(int data) {
+        head = new Node(data, head);
+        size++;
+    }
+
+    public void addLast(int data) {
+        if (head == null) {
+            addFirst(data);
+        } else {
+            Node current = head;
+            while (current.next != null) {
+                current = current.next;
+            }
+            current.next = new Node(data, null);
+            size++;
+        }
+    }
+
+    public int removeFirst() {
+        if (head == null) {
+            throw new NoSuchElementException();
+        }
+        int data = head.data;
+        head = head.next;
+        size--;
+        return data;
+    }
+
+    public int size() {
+        return size;
+    }
 }
